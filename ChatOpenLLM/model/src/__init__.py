00ce5f5ed9b0cf1edee06c698f00b81143d25b8e
Shafi2016 <56795978+Shafi2016@users.noreply.github.com>
2023-07-19 15:05:01 -0600

    Update __init__.py

--- ChatOpenLLM/model/src/__init__.py
+++ ChatOpenLLM/model/src/__init__.py
@@ -1,4 +1,5 @@
-from .src.Llama import Chat_Llama 
-from .src.LlamaGPTQ import ChatGPTQ
-from .src.autoModel import Chat_AutoModels
-from .src.functions_llm import create_tagging_chain2
+# ChatOpenLLM/model/src/__init__.py
+from .Llama import Chat_Llama
+from .LlamaGPTQ import ChatGPTQ
+from .autoModel import Chat_AutoModels
+from .functions_llm import create_tagging_chain2
